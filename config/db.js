919eec2f18eeff1f3f6d0bec954d3085205de1d8
hardwriter <39151999+hardwriter@users.noreply.github.com>
2022-03-23 18:55:31 +0800

    修改配置信息

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ module.exports = {
         host      : '127.0.0.1', // 服务器地址
         database  : 'melog', // 数据库名
         user      : 'root', // 数据库用户名
-        password  : '', // 数据库密码
-        port      : '3306', // 数据库连接端口
+        password  : '123456', // 数据库密码
+        port      : '3308', // 数据库连接端口
         charset   : 'utf8', // 数据库编码默认采用utf8
         prefix    : 'melog_' // 数据库表前缀
     }
